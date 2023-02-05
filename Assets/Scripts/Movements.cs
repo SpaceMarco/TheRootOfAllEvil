eba69da7c6124b429b629ea2054872bff910b5a4
SpaceMarco_WindowsPC <black1cl0ud99@gmail.com>
2023-02-05 05:18:27 +0200

    Added Local multiplayer added Jumping animations added Running animations added Health UI added one-way platforms added the new input system added jumping and falling in-code

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,18 @@ using Photon.Pun;
 
 public class Movements : MonoBehaviour
 {
+
     float moveHorizontal;
     float moveVertical;
     float lerp_acc = 0.5f;
     [SerializeField] float mvoement_speed;
     [SerializeField] float Jump_force;
+    [SerializeField] bool triggerShake = false;
     Vector2 Velocity; 
     Rigidbody2D rb;
+    Animator Anim;
+    SpriteRenderer spriteRender;
+    
 
     PhotonView view;
 
@@ -20,30 +26,70 @@ public class Movements : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         view = GetComponent<PhotonView>();
+        Anim = GetComponent<Animator>();
+        spriteRender = GetComponent<SpriteRenderer>();
     }
 
 
     void Update()
     {
-        if(view.IsMine)
-        {
+        if (!view.IsMine) return;
+
             moveHorizontal = Input.GetAxis ("Horizontal");
             moveVertical = Input.GetAxis ("Vertical");
+
+            if(moveHorizontal>0)
+            {
+                spriteRender.flipX = false;
+            }
+            else if(moveHorizontal<0)
+            {
+                spriteRender.flipX = true;
+            }
+
 
             Velocity = new Vector2(moveHorizontal * mvoement_speed, rb.velocity.y);
             
             if(Input.GetKeyDown(KeyCode.T))
             {
-                Camera.main.GetComponent<Shake>().shake_camera();
+                Anim.Play("punch"); 
             }
+
+            
 
 
             if(Input.GetKeyDown(KeyCode.Space))
             {
                 Velocity.y += 1f * Jump_force;
             }
+
+            if(moveHorizontal != 0 && moveVertical == 0) {
+                //Play your sideways animation
+                Anim.SetBool("isRunning",true);
+            }
+            else
+            {
+                Anim.SetBool("isRunning",false);
+            }
+
             rb.velocity = Velocity;
+
+        if(triggerShake)
+        {
+            Camera.main.GetComponent<Shake>().shake_camera();
         }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.IsWriting)
+		{
+			stream.SendNext(spriteRender.flipX);
+		}
+		else
+		{
+			spriteRender.flipX = (bool) stream.ReceiveNext();
+		}
     }
 
 }
